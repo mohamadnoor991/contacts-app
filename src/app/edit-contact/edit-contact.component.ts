7c7ad0a40c034e29db9a6dc8cd763038a6a0827b
mohamadnoor991 <mohamadbader20@yahoo.com>
2025-11-12 17:42:31 +0100

    add form control to edit-contact

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
+
+  firstName = new FormControl();
+  secondName = new FormControl();
+  dateOfBirth = new FormControl();
+  favoritesRanking = new FormControl();
+
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
@@ -16,6 +23,10 @@ export class EditContactComponent implements OnInit {
   }
 
   saveContact() {
-
+    console.log('Contact saved');
+    console.log('First Name:', this.firstName.value);
+    console.log('Last Name:', this.secondName.value);
+    console.log('Date of Birth:', this.dateOfBirth.value);
+    console.log('Favorites Ranking:', this.favoritesRanking.value);
   }
 }
